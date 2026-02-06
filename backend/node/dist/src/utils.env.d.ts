2d6eb21e5ea1866a5ef915c3353ca0d0065fc808
Kevin Medara <kmedara2015@outlook.com>
2026-02-05 23:07:36 -0500

    link and fetching items

--- backend/node/dist/src/utils.env.d.ts
+++
@@ -1,3 +0,0 @@
-import type { PlaidEnv } from "./plaid/types.js";
-export declare const getPlaidEnvVars: () => PlaidEnv;
-//# sourceMappingURL=utils.env.d.ts.map
--- backend/node/dist/utils.environment.d.ts
+++
@@ -1,3 +0,0 @@
-import type { PlaidEnv } from "./plaid/types.js";
-export declare const getPlaidEnvVars: () => PlaidEnv;
-//# sourceMappingURL=utils.environment.d.ts.map
